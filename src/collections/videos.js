c9cacf0943dfae2f5ce93c7d7beb1bc87b30c58d
ranched <jasosmc@yahoo.com>
2017-12-05 12:29:35 -0600

    Complete AppView tests

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,16 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
-  
+  model: Video,
+
+  search: function(searchData){
+    Backbone.ajax({
+      data: {
+        q: searchData
+      }
+    });
+  },
+
+  parse: function(paramsObj){
+    return paramsObj[Object.keys(paramsObj)[0]];
+  }
 });
--- src/views/app.js
+++ src/views/app.js
@@ -4,8 +4,18 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos();
+    this.render();
+    const videoList = new VideoListView({collection: this.videos});
+    const videoPlayer = new VideoPlayerView({collection: this.videos});
+    videoPlayer.render();
+    const searchView = new SearchView();
+    searchView.render();
+    this.videos.search();
+    console.log('out', this.videos);
+    this.listenTo(this.videos, 'sync', function(){
+      this.videos.models[0].select();
+    }.bind(this))
   },
-
 
   render: function() {
     this.$el.html(this.template());
